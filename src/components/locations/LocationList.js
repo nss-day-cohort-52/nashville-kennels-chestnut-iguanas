cf34072db9040ced39f38fe9207d0591977acd74
sherwin025 <svargas025@gmail.com>
2021-11-30 09:01:55 -0600

    show numbers

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -9,6 +9,9 @@ export const LocationList = () => {
 
     useEffect(() => {
         LocationRepository.getAll()
+        .then((res)=>{
+            updateLocations(res)
+        })
     }, [])
 
     return (
